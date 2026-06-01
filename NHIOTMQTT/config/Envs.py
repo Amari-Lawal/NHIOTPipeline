92b50409cc31218c0648d17dee5392b7397b050d
Amari <amari.lawal@gmail.com>
2026-06-01 17:29:04 +0100

    Fix: Add requests package and make .env path loading robust against execution contexts

--- NHIOTMQTT/config/Envs.py
+++ NHIOTMQTT/config/Envs.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
-load_dotenv("./NHIOTMQTT/.env")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+env_path = os.path.abspath(os.path.join(current_dir, "..", ".env"))
+load_dotenv(env_path)
 class Envs:
     ENDPOINT = os.getenv("ENDPOINT")
     CA_FILE = os.getenv("CA_FILE")
