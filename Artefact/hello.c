0a00d5b5b430875ae8da4b412eef7f77b15e9bf2
Amari <amari.lawal@gmail.com>
2026-08-23 17:37:53 +0100

    This is a test succes path.

--- Artefact/hello.c
+++ Artefact/hello.c
@@ -38,7 +38,7 @@ void crash(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
     // Real CPU floating point exception
-    // Hello
+    // Hello This is another test.
     volatile int a = 1;
     volatile int b = 0;
     volatile int c = a / b;
