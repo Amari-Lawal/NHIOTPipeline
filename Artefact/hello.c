872a2622f20f481f059ae91cf2608c7344d5cf0f
Amari <amari.lawal@gmail.com>
2026-08-26 19:36:16 +0100

    Trigger OTA Artifact Build via Admin Dashboard

--- Artefact/hello.c
+++ Artefact/hello.c
@@ -1,4 +1,4 @@
-// Dashboard OTA Build Trigger: 2026-08-26T19:29:55.581299
+// Dashboard OTA Build Trigger: 2026-08-26T19:36:16.116426
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,7 +19,7 @@ void minus(int argc, char *argv[]) {
     int result = atoi(argv[0]);
 
     for (int i = 1; i < argc; i++) {
-        result += atoi(argv[i]);
+        result *= atoi(argv[i]);
     }
 
     printf("minus:%d", result);
